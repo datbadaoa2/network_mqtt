0d51a11afa381c1e4c32eee66a37dc64b6a088fa
Tran Cong Viet An <trancongvietan22@gmail.com>
2021-12-19 21:32:38 +0700

    refactor(server): added gradle task for running server

--- src/main/java/test/ServerTest.java
+++ src/main/java/test/ServerTest.java
@@ -4,6 +4,7 @@ import mqtt.Server.Server;
 
 public class ServerTest {
     public static void main(String[] args){
+        System.out.println("Starting MQTT Server");
         Server server = new Server();
         server.loop();
     }
